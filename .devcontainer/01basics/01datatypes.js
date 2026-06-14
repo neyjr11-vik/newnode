8b4cd6caa2c304a151d5bdf46664059191977ef0
aditya vikram jha <avgopaljha@gmail.com>
2026-06-14 19:36:38 +0000

    container config done

---
+++ .devcontainer/01basics/01datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js codes as newer codes
+//alert(3+3) //we are using node ;not browser.
+console.log(3
+    + 
+    3
+)//code readability should be high;so dont use unusual spacings
+console.log("avj");
+let name="aditya"
+let age=21
+let IsLoggedIn=true;
+
+
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+//booleam => true/false
+//null => stalndalone value
+//undefined ==> value is not being assigned
+//symbol ==> uniqueness
+ 
+
+// object
+console.table([typeof "aditya",typeof 21,typeof true,typeof null/*uobject type*/,typeof undefined])
---
+++ .devcontainer/01basics/01variables.js
@@ -0,0 +1,15 @@
+const accountID=144553
+let accountEmail="avjhaokay@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+// accountiD=23
+console.log(accountID);
+accountEmail="adyajhaok@google.com"
+accountpassword="54321"
+accountCity="Delhi"
+let accountState;
+console.table([accountEmail,accountPassword,accountCity,accountState])
+/*prefer not to use var because of issue in block scope and functional scope
+
+
+*/
