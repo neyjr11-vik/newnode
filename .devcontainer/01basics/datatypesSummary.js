2be882b024329bed6fe95445e01345bcc3503c7e
aditya vikram jha <avgopaljha@gmail.com>
2026-06-15 19:28:44 +0000

    container config done

--- .devcontainer/01basics/datatypesSummary.js
+++ .devcontainer/01basics/datatypesSummary.js
@@ -32,3 +32,24 @@ const my_func=function(){
 const id=Symbol("123")
 const another_id=Symbol("123")
 console.log(id===another_id)
+
+//STACK AND HEAP
+//STACK-- FOR PRIMITIVE DATA TYPES;;;HEAP--FOR NON PRIMITIVE DATA TYPES
+//here ytname2=ytname gives a copy of ytname to be assigned,and not the exact ytname,,and hence any change in ytname2 doesnt affect ytname
+let ytname="avjha"
+let ytname2=ytname
+ytname2="aditya"
+console.log(ytname)
+console.log(ytname2)
+
+//here change in mydetails2 affects mydetails1,as in heap,it doesnt pass a copy,but it passes the exact same reference as my details
+
+
+let mydetails={
+    name:"avjha",
+    slno:21
+}
+let mydetails2=mydetails
+mydetails2.slno=22
+console.log(mydetails.slno)
+console.log(mydetails2.slno)
