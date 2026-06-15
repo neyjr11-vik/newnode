7065a4d4b6651d0d701cfac731ee041525299a66
aditya vikram jha <avgopaljha@gmail.com>
2026-06-15 09:41:34 +0000

    container config done

---
+++ .devcontainer/01basics/01comparisions.js
@@ -0,0 +1,13 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log("02">1)
+// console.log(null==0)
+// console.log(null>0)
+// console.log(null>=0)
+// console.log(undefined>=0 || undefined<=0)
+
+//console.log(true>0)
+
+// === :compares even the data type
+console.log("2"==2)
+console.log("2"===2)
