d8b49d816cdd788ceb7b32220fdeb6ba46a65168
aditya vikram jha <avgopaljha@gmail.com>
2026-06-15 08:37:57 +0000

    container config done

---
+++ .devcontainer/01basics/01conversions.js
@@ -0,0 +1,19 @@
+let score="33";
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+score="33abc"
+valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)//output==>Nan(Not A Number) as we convert 33abc into number
+console.log(typeof Number(null))
+console.log(Number(null))
+console.log(Number(undefined))
+//"33"==>33
+// "33abc"==>NaN
+// null==>0
+//true==>1;false==>0
+let IsLoggedIn=1
+console.log(Boolean(IsLoggedIn))
+console.log(String(345))
+console.log(typeof String(345))
