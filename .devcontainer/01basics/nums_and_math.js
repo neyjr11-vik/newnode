b2682f60e3c51c15cef109c8d9e47d902048690e
aditya vikram jha <avgopaljha@gmail.com>
2026-06-19 18:26:07 +0000

    container config done

---
+++ .devcontainer/01basics/nums_and_math.js
@@ -0,0 +1,29 @@
+const score=400
+console.log(score)
+const balance=new Number(400)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+const otherNum=1234.5678
+console.log(otherNum.toPrecision(6))
+const hundreds=100000000
+console.log(hundreds.toLocaleString())
+console.log(hundreds.toLocaleString('en-IN')) 
+
+
+
+// +++++++++++++++.  MATHS  .+++++++++++++++++
+
+
+ console.log(Math)
+ console.log(Math.abs(-4))
+ console.log(Math.round(4.6))
+ console.log(Math.ceil(4.2))
+ console.log(Math.floor(4.9))
+ console.log(Math.min(2,34,56,1))
+console.log(Math.max(23,12,24,56))
+console.log(Math.random()*10+1)
+console.log(Math.floor(Math.random()*10)+1)
+const min=20
+const max=30
+console.log(Math.floor(Math.random()*(max-min+1)+min))
